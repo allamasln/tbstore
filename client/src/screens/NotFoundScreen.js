e9a458472b3d363d6038001ad03bdd73b3c1d41f
Antonio Llamas <antonio@allamas.com>
2022-06-02 19:38:45 +0200

    Add 404 screen

---
+++ client/src/screens/NotFoundScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+function NotFoundScreen() {
+  return (
+    <h2 className="mt-5">
+      Oops! <strong>404</strong> Página no encontrada.
+    </h2>
+  )
+}
+
+export {NotFoundScreen}
