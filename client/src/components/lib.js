1abec44f5862c9f9166af2095be993b663155607
Antonio Llamas <antonio@allamas.com>
2022-06-07 11:41:00 +0200

    Add a responsive table component

--- client/src/components/lib.js
+++ client/src/components/lib.js
@@ -50,13 +50,104 @@ const FlexContainer = styled.div(
 
 const Link = styled(LinkRouter)({color: colors.link})
 
+const TableBase = styled.table({
+  borderCollapse: 'collapse',
+  tableLayout: 'fixed',
+})
+const THead = styled.thead({
+  position: 'sticky',
+  top: '100px',
+  zIndex: '1',
+
+  [mq.small]: {
+    position: 'fixed',
+    bottom: '60px',
+    right: '1rem',
+    top: 'initial',
+  },
+})
+
+const collapse = keyframes({
+  from: {
+    transform: 'scaleY(0)',
+    lineHeight: 0,
+  },
+  '40%': {
+    transform: 'scaleY(1)',
+    lineHeight: 1,
+  },
+})
+
+const zoomInOut = keyframes({
+  '0%': {transform: 'scale(1)'},
+  '30%': {transform: 'scale(0.98)'},
+})
+
+const TBody = styled.tbody({
+  zIndex: '0',
+  '>:nth-of-type(even)': {
+    backgroundColor: colors.gray10,
+  },
+  '>:hover:not([role="complementary"])': {
+    backgroundColor: colors.highlight,
+    cursor: 'pointer',
+  },
+  '>:active': {
+    animation: `${zoomInOut} ease .4s`,
+  },
+  [mq.small]: {
+    '>:nth-of-type(even):hover': {
+      animation: 'none',
+      backgroundColor: colors.gray10,
+    },
+    '>:nth-of-type(odd):hover': {
+      animation: 'none',
+      backgroundColor: 'inherit',
+    },
+  },
+})
+const TR = styled.tr({
+  [mq.small]: {
+    display: 'block',
+    marginBottom: '.625em',
+  },
+})
+const TH = styled.th({
+  fontSize: '0.75em',
+  letterSpacing: '0.1em',
+  textTransform: 'uppercase',
+  padding: '0.8rem 1rem',
+  background: colors.gray80,
+  color: colors.base,
+})
+
+const TD = styled.td({
+  padding: '0.9rem 1rem',
+  color: colors.black,
+  [mq.small]: {
+    borderBottom: '1px solid #ddd',
+    display: 'block',
+    fontSize: '.8em',
+    textAlign: 'right',
+    ':before': {
+      content: 'attr(data-label)',
+      float: 'left',
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+    },
+    ':last-of-type': {
+      borderBottom: 0,
+    },
+  },
+})
+
 const disabledButtonStyles = {
   true: {
     background: colors.gray10,
     color: colors.gray80,
   },
   false: {
-    background: colors.blueDarken,
+    background: colors.blueDarken20,
     color: colors.base,
     ':hover': {
       filter: 'brightness(120%)',
@@ -66,11 +157,11 @@ const disabledButtonStyles = {
 
 const Button = styled.button(
   {
-    backgroundColor: colors.blueDarken,
+    backgroundColor: colors.blueDarken20,
     border: '0',
     borderRadius: '3px',
     lineHeight: '1',
-    padding: '10px 15px',
+    padding: '0.8rem 1rem',
     fontWeight: 'bold',
   },
   ({disabled = 'false'}) => disabledButtonStyles[disabled],
@@ -126,7 +217,14 @@ export {
   Button,
   Input,
   SearchBox,
+  TableBase,
+  THead,
+  TBody,
+  TR,
+  TH,
+  TD,
   Spinner,
   Link,
   Pagination,
+  collapse,
 }
