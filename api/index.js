e90719f6a216eef1aa05f978b945cb390a395fa3
Antonio Llamas <antonio@allamas.com>
2022-06-15 09:58:21 +0200

    Fix status endpoints

--- api/index.js
+++ api/index.js
@@ -9,8 +9,8 @@ require('./startup/cors')(app)
 require('./startup/routes')(app)
 require('./startup/db')()
 
-app.get('/health', (req, res) => res.send('success'))
-app.get('/version', (req, res) => res.send(version))
+app.get('api/health', (req, res) => res.send('success'))
+app.get('api/version', (req, res) => res.send(version))
 
 const port = process.env.PORT || 3001
 const server = app.listen(port, () => winston.info(`Listening on ${port}...`))
